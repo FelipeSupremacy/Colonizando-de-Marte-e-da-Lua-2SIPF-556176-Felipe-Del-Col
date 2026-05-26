0c83cb6b440e43172acc892c0f2e8d1c448b0915
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-26 11:36:12 -0300

    Atualização

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,6 +2,11 @@ package app;
 
 import service.SistemaColonizacao;
 
+// Nome: Felipe Del Col
+// RM: 556176
+// Nome do Projeto escolhido: “Colonização de Marte e da Lua
+
+
 public class Main {
 
     public static void main(String[] args) {
